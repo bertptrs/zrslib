a981732fbf61143d6cfb25d12b35c49e2aff5064
Bert Peters <bert.ljpeters@gmail.com>
2016-04-18 17:36:33 +0200

    Implement initial getBuildings method.

---
+++ src/ZRSReader.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace zrslib;
+
+use DOMDocument;
+use DOMNodeList;
+use DOMXPath;
+use GuzzleHttp\Client;
+
+/**
+ * Reader for the ZRS system.
+ *
+ * @author Bert Peters
+ */
+class ZRSReader
+{
+    const ZRS_LOCATION     = "https://zrs.leidenuniv.nl/ul/";
+    const SEARCH_LOCATION  = "start.php";
+    const RESULTS_LOCATION = "query.php";
+
+    /**
+     * @var Client http client to use.
+     */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public static function getInstance()
+    {
+        $client = new Client();
+
+        return new ZRSReader($client);
+    }
+
+    public function getBuildings()
+    {
+        $document = $this->getSearchPage();
+        echo $document->saveHTML();
+        $xpath    = new DOMXPath($document);
+
+        /* @var $options DOMNodeList */
+        $options = $xpath->query("//select[@name=\"selgebouw\"]/option");
+
+        return $options->length;
+    }
+
+    /**
+     * Get the search page as a DOM document.
+     *
+     * @return DOMDocument
+     */
+    private function getSearchPage()
+    {
+        $url = self::ZRS_LOCATION.self::SEARCH_LOCATION;
+
+        $response = $this->client->get($url);
+        $contents = $response->getBody()->getContents();
+
+        return $this->parseHTML($contents);
+    }
+
+    /**
+     * Parse a html document.
+     *
+     * This method disables the built-in error handling to suppress it, and
+     * restores it afterwards.
+     *
+     * @param string $htmlContents
+     * @return DOMDocument
+     */
+    private function parseHTML($htmlContents)
+    {
+        $previous = libxml_use_internal_errors(true);
+        $document = DOMDocument::loadHTML($htmlContents);
+        libxml_use_internal_errors($previous);
+
+        return $document;
+    }
+}
---
+++ tests/unit/ZRSReaderTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace zrslib\tests\unit;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Test cases for the ZRS reader class.
+ *
+ * @author Bert Peters
+ */
+class ZRSReaderTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetBuildings()
+    {
+        $instance = \zrslib\ZRSReader::getInstance();
+
+        $result = $instance->getBuildings();
+
+        $this->assertEquals(64, $result);
+
+        $this->fail("This is just a dummy test.");
+    }
+}
